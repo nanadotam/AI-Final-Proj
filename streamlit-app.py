d4751a10c3dc9ba0e102f6d6e9714d16d0bd6e18
nanadotam <nanaamoako202@gmail.com>
2024-08-05 19:38:37 +0000

    Current price working

--- streamlit-app.py
+++ streamlit-app.py
@@ -13,7 +13,7 @@ data_path = 'data/preprocessed_ICCO_daily_prices.csv'
 
 # Check if paths exist
 if not os.path.exists(model_path):
-    st.error(f"Model file not found: {model_path}") 
+    st.error(f"Model file not found: {model_path}")
 if not os.path.exists(scaler_path):
     st.error(f"Scaler file not found: {scaler_path}")
 if not os.path.exists(data_path):
@@ -21,7 +21,8 @@ if not os.path.exists(data_path):
 
 # Load the trained model and scaler
 model = load_model(model_path, compile=False)
-scaler = joblib.load(scaler_path)
+with open(scaler_path, 'rb') as f:
+    scaler = joblib.load(f)
 
 # Ensure the feature names match
 feature_columns = ['ICCO daily price (US$/tonne)', 'MA_7', 'MA_30', 'MA_90', 'Price_Diff', 'Volatility_7', 'Volatility_30', 'Volatility_90']
@@ -47,7 +48,7 @@ df['Date'] = pd.to_datetime(df['Date'])
 st.title('Cocoa Price Prediction')
 
 st.header('Current Price')
-latest_price = df.iloc[-1]['New York futures (US$/tonne)']
+latest_price = df.iloc[0]['New York futures (US$/tonne)']
 st.write(f"Current Price: ${latest_price}")
 
 st.header('Predict Cocoa Prices for the Next 30 Days')
@@ -60,9 +61,6 @@ if st.button('Predict'):
     # Inverse transform the predictions
     predictions = scaler.inverse_transform(np.concatenate([predictions.reshape(-1, 1), 
                                                             np.zeros((forecast_days, len(feature_columns) - 1))], axis=1))[:, 0]
-    
-    # Convert predictions to thousands of dollars
-    # predictions = predictions / 1000
 
     latest_date = pd.to_datetime(df['Date']).max()
     dates = pd.date_range(start=latest_date, periods=forecast_days + 1).tolist()[1:]
@@ -72,8 +70,7 @@ if st.button('Predict'):
     fig.update_layout(
         title='Cocoa Price Prediction for Next 30 Days', 
         xaxis_title='Date', 
-        yaxis_title='Price (Thousands of US$/tonne)'
-        # yaxis=dict(tickformat='$,.2f')  
+        yaxis_title='Price (US$/tonne)'
     )
     
     st.plotly_chart(fig)
@@ -84,7 +81,7 @@ end_date = st.date_input('End Date')
 
 if st.button('View Prices'):
     mask = (df['Date'] >= pd.to_datetime(start_date)) & (df['Date'] <= pd.to_datetime(end_date))
-    historical_data = df.loc[mask] 
+    historical_data = df.loc[mask]
 
     dates = historical_data['Date'].tolist()
     prices = historical_data['New York futures (US$/tonne)'].tolist()
